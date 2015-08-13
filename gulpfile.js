f22b0f10cf534199373b3fa49e0c8258d5213f7c
Masato Yoshioka <yoshioka0627@gmail.com>
2015-08-13 12:04:23 +0900

    add gulp

---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+'use strict';
+
+var gulp = require("gulp");
+var watchify = require("gulp-watchify");
+
+var less = require("gulp-less");
+
+gulp.task("build", [
+    "build:less",
+]);
+
+gulp.task("build:less", function(){
+    return gulp.src("./src/less/2048.less")
+            .pipe(less())
+            .pipe(gulp.dest("./www/dist/css"));
+});
+
+var watching = false;
+gulp.task("enable-watch-mode", function(){
+    watching = true;
+});
+gulp.task("browserify", watchify(function(watchify){
+    return gulp.src("./src/app/app.js")
+            .pipe(watchify({
+                watch: watching
+            }))
+            .pipe(gulp.dest("./www/dist/js"));
+}));
+gulp.task("watch", ["build", "enable-watch-mode", "browserify" ], function(){
+    gulp.watch("./src/less/**/*.less", ["build:less"] );
+});
+
+gulp.task("default", ['build', "browserify"]);
